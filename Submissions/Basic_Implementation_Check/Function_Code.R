7ac09e05da0c59468316750e0d3bea8e83166048
Valerie Pan <valeriepan3@gmail.com>
2026-02-14 16:50:14 -0500

    Adjustments to function... Still some problems error rates and power need to be resolved.

--- Submissions/Basic_Implementation_Check/Function_Code.R
+++ Submissions/Basic_Implementation_Check/Function_Code.R
@@ -1,99 +1,47 @@
 
 
-
 # A basic version, without any functions yet
 # add some adjustable parameters when we make the function
 
-# 1. generate n binomial samples
-set.seed(1)
-n <- 10^4 # number of simulated values
+# note on parameters in the function: 
+# N: number of simulated values
+# n: total trials in binomial 
+# p_true: true probability of success in the binomial population
+# p_0: null hypothesis
 
-# just chose some random values for now
-m <- 10 # total trials in binomial 
-p_true <- 0.5 # true probability of success in the binomial population
+# This function calculates the p-values of Monte Carlo simulated binomial samples, 
+# returns the pvalues, results of hypothesis test (in rejection), type 1 error rate, type 2 error rate and power
 
-x <- rbinom(n, size = m, p = p_true) # draw a random sample x with n number of simulated binomial values
+MCsim_binom <- function(seed = 1, N = 10000, n = 100, p_true = 0.5, p_0 = 0.5, alpha = 0.05) { # provided default values
+  set.seed(seed)
+  
+  # 1. generate data 
+  x <- rbinom(N, n, p_true) # draw a random sample x with n number of simulated binomial values
+  
+  # 2. Calculate p_hat for ALL samples (vectorized -- we don't need a for loop in this case)
+  p_hat <- x / n # sample proportion, sample successes / number of bin trials
+  
+  # 3. Calculate z for ALL samples (vectorized)
+  se_0 <- sqrt(p_0 * (1 - p_0) / n) # standard error
+  z <- (p_hat - p_0) / se_0 # z-test stat
 
-p_0 <- 0.7 # define some null hypothesis proportion
-
-# 2. Apply z-test to each sample
-for(i in 1:n){
-  p_hat <- x[i]/m # sample proportion, probability of sample successes / number of bin trials  
+  # 4. Calculate p-values for ALL samples (vectorized)
+  p_val <- 2 * (1 - pnorm(abs(z))) 
   
-  z <- (p_hat - p_0) / sqrt(p_0*(1-p_0)/m) # calculate z-test statistic
+  # 5. apply "Hit or Miss" (check rejection of null hypothesis)
+  rejections <- (p_val < alpha)
   
-  # add the hypothesis test here...
+  # 6. calculate error rates and power: 
   
-}
-
-#Verification of Type I Error
-#' Goal: When H0 is true, is the rejection rate close to alpha?
-verify_type_i_error <- function(N = 10000, n = 100, p_true = 0.5, p_0 = 0.5, alpha = 0.05) {
-  
-  # 1. Generate Data (Vectorized)
-  X <- rbinom(N, n, p_true)
-  
-  # 2. Calculate Sample Proportions
-  p_hat <- X / n
-  
-  # 3. Calculate Z-scores (The Test)
-  se_0 <- sqrt(p_0 * (1 - p_0) / n)
-  Z <- (p_hat - p_0) / se_0
-  
-  # 4. Calculate P-values
-  P_val <- 2 * (1 - pnorm(abs(Z)))
-  
-  # 5. Apply "Hit or Miss" (Check Rejections)
-  rejections <- P_val < alpha
-  
-  # 6. Calculate Verification Metric
-  type_i_error_rate <- mean(rejections)
-  
-  # Verification (Pass/Fail)
-  if (abs(type_i_error_rate - alpha) < 0.01) {
-    # Rejection rate is close to alpha
-  } else {
-    # Check for issues
-  }
-  
-  return(type_i_error_rate)
-}
-
-#Verification of Power & Type II Error
-verify_power_type_ii <- function(N = 10000, n = 100, p_true = 0.7, p_0 = 0.5, alpha = 0.05) {
-  
-  # 1. Generate Data
-  X <- rbinom(N, n, p_true)
-  
-  # 2. Calculate Statistics
-  p_hat <- X / n
-  se_0 <- sqrt(p_0 * (1 - p_0) / n)
-  Z <- (p_hat - p_0) / se_0
-  P_val <- 2 * (1 - pnorm(abs(Z)))
-  
-  # 3. Apply "Hit or Miss" (Check Rejections)
-  rejections <- P_val < alpha
-  
-  # 4. Calculate Verification Metrics
-  power <- mean(rejections)
+  type_i_error_rate <- mean(rejections) 
+  # type 1 error rate (rejection rate)# when p_true = p_0
+  power <- mean(rejections) # when p_true does not equal p_0 
   type_ii_error_rate <- 1 - power
   
-  # Verification (Pass/Fail)
-  if (power > 0.95) {
-    # Power is high as expected
-  } else {
-    # Power is unexpectedly low
-  }
+  return(list(p_values = p_val_vec, rejections = rejections, type_i_error_rate = type_i_error_rate, power = power, type_ii_error_rate = type_ii_error_rate))
   
-  return(list(power = power, type_ii = type_ii_error_rate))
 }
 
-# Run Verifications
-set.seed(380) # For reproducibility
-type_i_res <- verify_type_i_error()
-power_res <- verify_power_type_ii()
-
-# Display results
-type_i_res
-power_res
-
+# Checking that the functions work: 
+head(MCsim_binom()$p_values)
+head(MCsim_binom()$rejections)
