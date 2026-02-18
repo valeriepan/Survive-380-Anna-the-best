9e6541978d22d890cd52ff12718f057787808603
Tianhong Shen <st6552659@gmail.com>
2026-02-17 20:49:59 -0500

    update function that can compute type I, type II and power of the test(not include the case when alternative hypothesis is not well-defined)

--- Submissions/Basic_Implementation_Check/Function_Code.R
+++ Submissions/Basic_Implementation_Check/Function_Code.R
@@ -1,70 +1,80 @@
-
-
 # A basic version, without any functions yet
 # add some adjustable parameters when we make the function
 
 # note on parameters in the function: 
 # N: number of simulated values (Simulations)
-# n: total trials in binomial (Sample Size per Simulation)
-# p_true: true probability of success in the binomial population
+# sample_size: total trials in binomial (Sample Size per Simulation)
 # p_0: null hypothesis
 
-# This function calculates the p-values of Monte Carlo simulated binomial samples, 
-# returns the pvalues, results of hypothesis test (in rejection), type 1 error rate, type 2 error rate and power
+# This function calculates the type I error rate using Monte Carlo simulated binomial samples, 
+# returns the estimated type I error rate and Monte Carlo standard error
 
-MCsim_binom <- function(seed = 1, N = 10000, n = 100, p_true = 0.5, p_0 = 0.5, alpha = 0.05, alternative = "two.sided") { # provided default values
-
-  set.seed(seed)
-
-  # 1. generate data 
-  x <- rbinom(N, n, p_true) # draw a random sample x with n number of simulated binomial values
+mc_type1_binom <- function(N = 10^5, 
+                           sample_size,
+                           p_0, 
+                           alpha = 0.05, 
+                           alternative = c("greater", "less", "not_equal")) { # provided default variables
+  alternative <- match.arg(alternative)
   
-  # 2. Calculate p_hat for ALL samples (vectorized -- we don't need a for loop in this case)
+  # 1. simulation under H_0 : x ~ Bin(sample_size, p_0) 
+  x <- rbinom(N, size = sample_size, prob = p_0)
+  
+  # 2. compute z-statistic under normal approximation
   p_hat <- x / n # sample proportion, sample successes / number of bin trials
+  z <- (p_hat - p_0)/sqrt((p_0 * (1 - P_0))/n) # computing z-statistic
   
-  # 3. Calculate z for ALL samples (vectorized)
-  se_0 <- sqrt(p_0 * (1 - p_0) / n) # standard error
-  z <- (p_hat - p_0) / se_0 # z-test stat
+  # 3. Rejection rule based on the chosen alternative
+  reject <- switch(
+    alternative,
+    greater   = z >  qnorm(1 - alpha),
+    less      = z <  qnorm(alpha),
+    not_equal = abs(z) > qnorm(1 - alpha / 2)
+  )
   
-  # 4. Calculate p-values for ALL samples (vectorized)
-  if (alternative == "two.sided") {
-    p_val <- 2 * (1 - pnorm(abs(z)))
-  } else if (alternative == "less") {
-    p_val <- pnorm(abs(z))
-  } else if (alternative == "greater") {
-    p_val <- 1 - pnorm(abs(z))
-  }
-  
-  # 5. apply "Hit or Miss" (check rejection of null hypothesis)
-  rejections <- (p_val < alpha)
-  
-  # 6. calculate error rates and power based on truth
-  if(p_true == p_0) {
-    # H0 is True: Calculate Type I Error
-    type_i_error_rate <- mean(rejections)
-    power <- NULL
-    type_ii_error_rate <- NULL
-    
-  } else {
-    # H0 is False: Calculate Power and Type II Error
-    power <- mean(rejections)
-    type_ii_error_rate <- 1 - power
-    type_i_error_rate <- NULL
-
-  }
-
-  return(list(
-    type_i_error = type_i_error_rate, 
-    power = power, 
-    type_ii_error = type_ii_error_rate, 
-    p_values = p_val,      
-    rejections = rejections,
-    z_scores = z           
-  ))
+  # 4. Return the estimated type I error rate and Monte Carlo standard error of estimated type I error rate
+  return(list(type1_hat <- mean(reject), # Estimated type I error rate
+         mc_se <- sqrt(type1_hat * (1 - type1_hat) / m))) # SE for type I error
+  # in case we need this
 }
 
-res1 <- MCsim_binom(p_true = 0.5, p_0 = 0.5, alternative = "two.sided")
-print(paste("Type I Error:", res1$type_i_error))
 
-res2 <- MCsim_binom(p_true = 0.6, p_0 = 0.5, alternative = "greater")
-print(paste("Power:", res2$power))
+
+# note on parameters in the function: 
+# N: number of simulated values (Simulations)
+# sample_size: total trials in binomial (Sample Size per Simulation)
+# p_0: null hypothesis
+
+# This function calculates the type II error rate and power of the test
+# using Monte Carlo simulated binomial samples, 
+# returns the estimated type II error rate, power of the test and Monte Carlo standard error for both
+
+# Notice : this function calculate type II error rate and power when alternative hypothesis is well-define(i.e. p = p_1)
+#if the alternative is not well-define, we have to compute the whole power curve !!!
+mc_power_simple_binom <- function(N, p_0, p_1, 
+                                  alpha = 0.05, sample_size = 10^5,
+                                  alternative = c("greater", "less", "two.sided")) {
+  alternative <- match.arg(alternative)
+  
+  # 1. Simulate under the alternative truth: x ~ Bin(sample_size, p_1)
+  x <- rbinom(m, size = n, prob = p1)
+  
+  # 2. compute z-statistic under normal approximation
+  p_hat <- x / n # sample proportion, sample successes / number of bin trials
+  z <- (p_hat - p_0)/sqrt((p_0 * (1 - P_0))/n) # computing z-statistic
+  
+  # 3. Rejection rule based on the chosen alternative
+  reject <- switch(
+    alternative,
+    greater   = z >  qnorm(1 - alpha),
+    less      = z <  qnorm(alpha),
+    two.sided = abs(z) > qnorm(1 - alpha / 2)
+  )
+  
+  power_hat <- mean(reject)
+  type2_hat <- 1 - power_hat
+  
+  # Monte Carlo standard errors
+  return(list(power_mc_se <- sqrt(power_hat * (1 - power_hat) / m), # SE for estimated power
+              type2_mc_se <- power_mc_se))  # SE for estimated type II error
+  
+}
